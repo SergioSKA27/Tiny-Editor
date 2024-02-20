5226b66cc24c8ee2818613f8e130ab246e76e9d2
SergioSKA27 <sergioska81@hotmail.com>
2024-02-20 06:24:29 -0600

    Agregado archivo setup.py con configuración del paquete st-tiny-editor

---
+++ setup.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="st-tiny-editor",
+    version="0.0.1",
+    author="Sergio Demis Lopez Martinez",
+    author_email="sergioska81@hotmail.com",
+    description="Streamlit component that allows you to render a TinyMCE editor in your app.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=[
+        "streamlit >= 0.63",
+    ],
+    extras_require={
+        "devel": [
+            "wheel",
+            "pytest==7.4.0",
+            "playwright==1.39.0",
+            "requests==2.31.0",
+            "pytest-playwright-snapshot==1.0",
+            "pytest-rerunfailures==12.0",
+        ]
+    },
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Software Development :: User Interfaces",
+    "Topic :: Software Development :: Text Editors",
+    "Topic :: Software Development :: Libraries :: Application Frameworks",
+    "Topic :: Editor :: TinyMCE",
+    ]
+)
