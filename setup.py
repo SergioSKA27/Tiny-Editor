b6c6bf4347ddf726d9627565bfc02aa641578c4b
SergioSKA27 <sergioska81@hotmail.com>
2024-02-20 06:44:01 -0600

    Actualización de la versión a 1.0.1 y eliminación de clasificadores

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="st-tiny-editor",
-    version="1.0.0",
+    version="1.0.1",
     author="Sergio Demis Lopez Martinez",
     author_email="sergioska81@hotmail.com",
     description="Streamlit component that allows you to render a TinyMCE editor in your app.",
@@ -30,15 +30,5 @@ setuptools.setup(
             "pytest-rerunfailures==12.0",
         ]
     },
-    classifiers=[
-    "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-    "Intended Audience :: Developers",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    "Topic :: Software Development :: User Interfaces",
-    "Topic :: Software Development :: Text Editors",
-    "Topic :: Software Development :: Libraries :: Application Frameworks",
-    "Topic :: Editor :: TinyMCE",
-    ]
+    classifiers=[]
 )
