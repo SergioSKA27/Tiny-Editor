29a6954fd9a39a32a7ab14ea47410b492374660a
SergioSKA27 <sergioska81@hotmail.com>
2024-02-20 06:04:31 -0600

    Actualización de la funcionalidad del editor de texto y eliminación de pruebas de plantillas

--- app.py
+++ app.py
@@ -6,8 +6,4 @@ d = tiny_editor(st.secrets["TINY_API_KEY"],
 height=1000,
 initialValue="<p>Hello World</p>",
 toolbar = 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat')
-
-
-
-
 st.write(d,unsafe_allow_html=True)
--- st_tiny_editor/example.py
+++ st_tiny_editor/example.py
@@ -1,28 +1,10 @@
+from st_tiny_editor import tiny_editor
 import streamlit as st
-from my_component import my_component
 
-# Add some test code to play with the component while it's in development.
-# During development, we can run this just as we would any other Streamlit
-# app: `$ streamlit run my_component/example.py`
 
-st.subheader("Component with constant args")
+d = tiny_editor(st.secrets["TINY_API_KEY"],
+height=1000,
+initialValue="<p>Hello World</p>",
+toolbar = 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat')
 
-# Create an instance of our component with a constant `name` arg, and
-# print its output value.
-num_clicks = my_component("World")
-st.markdown("You've clicked %s times!" % int(num_clicks))
-
-st.markdown("---")
-st.subheader("Component with variable args")
-
-# Create a second instance of our component whose `name` arg will vary
-# based on a text_input widget.
-#
-# We use the special "key" argument to assign a fixed identity to this
-# component instance. By default, when a component's arguments change,
-# it is considered a new instance and will be re-mounted on the frontend
-# and lose its current state. In this case, we want to vary the component's
-# "name" argument without having it get recreated.
-name_input = st.text_input("Enter a name", value="Streamlit")
-num_clicks = my_component(name_input, key="foo")
-st.markdown("You've clicked %s times!" % int(num_clicks))
+st.write(d,unsafe_allow_html=True)
