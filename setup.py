96620e08142aba16ed1885ef4c89dae118d01514
SergioSKA27 <sergioska81@hotmail.com>
2024-02-20 06:31:47 -0600

    Eliminar clasificadores no utilizados en setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setuptools.setup(
     url="",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    classifiers=[],
     python_requires=">=3.7",
     install_requires=[
         "streamlit >= 0.63",
