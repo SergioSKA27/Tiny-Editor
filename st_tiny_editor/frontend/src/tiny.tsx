c40778fe0329f5620f25b0663badd96b88079885
SergioSKA27 <sergioska81@hotmail.com>
2024-02-20 04:44:41 -0600

    Añadir referencia a divRef en tiny.tsx

--- st_tiny_editor/frontend/src/tiny.tsx
+++ st_tiny_editor/frontend/src/tiny.tsx
@@ -10,8 +10,9 @@ function App(props: ComponentProps) {
     const {apiKey, initialValue, height, menubar, plugins, toolbar, content_style,disabled} = props.args;
     useEffect(() => Streamlit.setFrameHeight(height));
     const editorRef = useRef<any>(null);
+    const divRef = useRef<HTMLDivElement>(null)
     return (
-      <div>
+      <div ref={divRef}>
         <Editor
           apiKey={apiKey}
           onInit={(evt, editor) => editorRef.current = editor}
@@ -21,7 +22,8 @@ function App(props: ComponentProps) {
             menubar: menubar,
             plugins: plugins,
             toolbar: toolbar,
-            content_style: content_style
+            content_style: content_style,
+            ai_request: (request: any, respondWith:any ) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant"))
           }}
           onEditorChange={(value,editor) => Streamlit.setComponentValue(value)}
           disabled={disabled}
