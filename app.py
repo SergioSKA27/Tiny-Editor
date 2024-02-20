af5060ce51d05f095b63cc00c512078761f0b008
SergioSKA27 <sergioska81@hotmail.com>
2024-02-20 05:22:35 -0600

    Actualización de .gitignore y uso de secretos de Streamlit en app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from st_tiny_editor import tiny_editor
 import streamlit as st
 
 
-d = tiny_editor('rbmpkoj5pz0ftietc5wnx0tei3dofocexwnxtrc9bvkl61aq',
+d = tiny_editor(st.secrets["TINY_API_KEY"],
 height=1000,
 initialValue="<p>Hello World</p>",
 toolbar = 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat')
